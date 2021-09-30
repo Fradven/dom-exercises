e8b6be6c13951874a6d28b53270901221fd4c65c
Fradven <frederic.vanovermeire@gmail.com>
2021-09-30 15:33:12 +0200

    finished

--- Selector/script.js
+++ Selector/script.js
@@ -1,7 +1,30 @@
+function rdmcolor(){
+  let r = function () {return Math.floor(Math.random()*256)}
+  return "rgb(" + r() + "," + r() + "," + r()+ ")"
+}
 
 
-function tool(link) {
-    link.title = 'after click';
+
+
+const important = document.querySelectorAll('.important');
+for (item of important) {
+    item.title = "This is an important item";
+}
+
+const images = document.querySelectorAll('img')
+for (item of images) {
+  if (item.classList.contains('important')) {
+    item.style.display = "none"
   }
+}
 
-  querySelectorAll.class = tool(important)
+const paragraph = document.querySelectorAll('p')
+for (item of paragraph) {
+  if (item.hasAttribute('class')) {
+    console.log(item.classList)
+  }
+  else {
+    item.style.color = rdmcolor()
+    console.log(item)
+  }
+}
