ea512f1b815093a92860070f18951bdb20dc5613
Fradven <frederic.vanovermeire@gmail.com>
2021-09-30 15:33:47 +0200

    modification to rgb

--- Intro/script.js
+++ Intro/script.js
@@ -10,7 +10,6 @@ document.body.style.backgroundColor = backcolor()
 
 function backcolor(){
     let r = function () {return Math.floor(Math.random()*256)}
-    let g = function () {return Math.floor(Math.random()*256)}
-    let b = function () {return Math.floor(Math.random()*256)}
-    return "rgb(" + r() + "," + g() + "," + b()+ ")"
+    return "rgb(" + r() + "," + r() + "," + r()+ ")"
 }
+console.log(backcolor())
